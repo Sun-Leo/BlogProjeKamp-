5e89cd4aad6318b87801b168b0ec119d1ff10245
Samsung <Samsung@DESKTOP-JKQEAD5>
2023-04-08 13:31:49 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterManager : IGenericServices<Writer>
+    {
+        private readonly IWriterDal _writerDal;
+
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+
+        public void TAdd(Writer t)
+        {
+            _writerDal.Add(t);
+        }
+
+        public void TDelete(Writer t)
+        {
+            _writerDal.Delete(t);
+        }
+
+        public Writer TGetById(int id)
+        {
+            return _writerDal.GetById(id);
+
+        }
+
+        public List<Writer> TGetList()
+        {
+            return _writerDal.GetList();
+        }
+
+        public void TUpdate(Writer t)
+        {
+            _writerDal.Update(t);
+        }
+    }
+}
